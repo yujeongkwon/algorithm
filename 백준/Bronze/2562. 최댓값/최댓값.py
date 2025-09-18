1015a66bb3fc4db7ae15122a98bc8cf4249b0adc
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2025-09-18 16:00:18 +0900

    [Bronze III] Title: 최댓값, Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,15 @@
+q = int(input())
+w = int(input())
+e = int(input())
+r = int(input())
+t = int(input())
+y = int(input())
+u = int(input())
+i = int(input())
+o = int(input())
+
+a=[]
+a = q,w,e,r,t,y,u,i,o
+a
+print(max(a))
+print(a.index(max(a))+1)
