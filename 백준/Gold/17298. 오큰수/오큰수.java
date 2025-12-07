cc0ecc007f2a74cc2e19fb3f22307fea51929718
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2025-12-07 18:27:45 +0900

    [Gold IV] Title: 오큰수, Time: 816 ms, Memory: 162740 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,29 +1,37 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] answer =  new int[N]; 
-        LinkedList<int[]> stack = new LinkedList<>();
+        LinkedList<Integer> stack = new LinkedList<>();
+        int[] answer = new int[N];
+
+        int[] nums = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int cur = Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty() && stack.peek()[0]< cur) {
-                int[] out = stack.pop();
-                answer[out[1]] = cur;
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=N-1; i>=0; i--) {
+
+            while(!stack.isEmpty() && stack.peek()<= nums[i]){
+                stack.pop();
             }
-            stack.push(new int[]{cur,i});
+            if(stack.isEmpty()) {
+                answer[i] = -1;
+            }
+            else {
+                answer[i] = stack.peek();
+            }
+            stack.push(nums[i]);
+
         }
-        while(!stack.isEmpty()) {
-            answer[stack.pop()[1]] = -1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(answer[i]).append(' ');
         }
-        
-        String s = Arrays.stream(answer)
-            .mapToObj(String::valueOf)
-            .collect(Collectors.joining(" "));
-        System.out.print(s);
+        System.out.println(sb.toString());
     }
 }
