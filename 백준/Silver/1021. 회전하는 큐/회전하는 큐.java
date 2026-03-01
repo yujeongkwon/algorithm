5593344b25f4d4fe845cbac9a4b97f3e77b64ea2
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2026-03-01 19:18:51 +0900

    [Silver III] Title: 회전하는 큐, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -8,35 +8,28 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
-        LinkedList<Integer> queue = IntStream.rangeClosed(1,N)
-            .boxed()
-            .collect(Collectors.toCollection(LinkedList::new));
+        int result = 0;
+        LinkedList<Integer> nums = new LinkedList<>(IntStream.rangeClosed(1,N)
+                                                    .boxed()
+                                                   .collect(Collectors.toList()));
         
         st = new StringTokenizer(br.readLine());
-        int answer = 0;
         for(int i=0; i<M; i++) {
-            int seek = Integer.parseInt(st.nextToken());
-            int left = queue.indexOf(seek);
-            int right = queue.size()-left;
+            int want =  Integer.parseInt(st.nextToken());
             
-            if(left<=right){
-                answer+=left;
-                for(int l=0; l<left; l++) {
-                    queue.offer(queue.poll());
-                }
-                
+            int idx = nums.indexOf(want);
+            if(idx <= nums.size() - idx) {
+                result += idx;
+                for(int j=0; j<idx; j++)    nums.offer(nums.poll());
+                nums.poll();
             }
             else {
-                answer+=right;
-                for(int r=0; r<right; r++) {
-                    queue.offerFirst(queue.pollLast());
-                }
-               
+                result += (nums.size() - idx);
+                for(int j=0; j<nums.size() - idx; j++)    nums.offerFirst(nums.pollLast());
+                nums.poll();
             }
-            queue.poll();
         }
-                           
-        System.out.print(answer);
+        
+        System.out.print(result);
     }
 }
