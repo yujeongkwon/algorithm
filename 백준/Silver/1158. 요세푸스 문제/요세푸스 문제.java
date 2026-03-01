5425f2aeeb97c71735b99728852ff13cdd67dc3f
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2026-03-01 16:38:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 244 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        LinkedList<Integer> list = new LinkedList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
         sb.append('<');
-        for(int i=1; i<=N; i++) {
-            list.offer(i);
-        }
-
-        int k =K;
-        while(!list.isEmpty()) {
-            if(k==1) {
-                sb.append(list.poll());
-                sb.append(", ");
-                k=K;
+        
+        LinkedList<Integer> list = new LinkedList<>(IntStream.rangeClosed(1,n)
+                                                    .boxed()
+                                                    .collect(Collectors.toList()));
+        ListIterator<Integer> it = list.listIterator();
+        int cur = 0;
+        while(list.size() != 1) {
+            if(!it.hasNext()) it = list.listIterator();
+            
+            int val = it.next();
+            cur++;
+            
+            if(cur==k) {
+                sb.append(val).append(", ");
+                it.remove();
+                cur=0;
             }
-            else {
-                list.offer(list.poll());
-                k--;
-            }
         }
-        sb.delete(sb.length()-2, sb.length());
-        sb.append('>');
-        System.out.print(sb.toString());
+        
+        sb.append(list.get(0)).append('>');
+        System.out.println(sb.toString());
     }
+
 }
